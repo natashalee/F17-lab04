2849681b4b64447b79dea9533af8d27857a27188
Wei Tung Chen <weitung_chen@umail.ucsb.edu>
2017-10-25 22:00:02 -0700

    Import sample classes and create 2 new classes under my working directory

---
+++ src/edu/ucsb/cs56/drawings/weitung_chen/advanced/Saturn.java
@@ -0,0 +1,31 @@
+package edu.ucsb.cs56.drawings.weitung_chen.advanced;
+import java.awt.geom.GeneralPath;  
+import java.awt.Shape; 
+import java.awt.geom.Ellipse2D;
+
+/**
+   A Saturn extended from Planet
+      
+   @author Wei Tung Chen
+   @version for CS56, F17, UCSB
+   
+*/
+public class Saturn extends Planet implements Shape
+{
+    /**
+     * Constructor for objects of class Saturn
+     */
+    public Saturn(double x, double y, double r)
+    {
+	//Call super class to construct planet
+	super(x,y, r);
+
+	//Construct saturn ring
+	double d = 2 * r;
+	Ellipse2D.Double ring = new Ellipse2D.Double(x - r, y + r, 2 * d, r/10);
+
+	//Construct whole saturn
+        GeneralPath wholeSaturn = this.get();
+        wholeSaturn.append(ring, false);
+    }    
+}
