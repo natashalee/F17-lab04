71f50689a0045a9eb18cfa40d6c3d0de0a794828
Nicholas Duncan <ncduncan@umail.ucsb.edu>
2017-10-27 21:02:49 -0700

    javadoc updated

--- src/edu/ucsb/cs56/drawings/ncduncan/SimpleGui1.java
+++ src/edu/ucsb/cs56/drawings/ncduncan/SimpleGui1.java
@@ -8,7 +8,7 @@ import javax.swing.*;
     @author Head First Java, 2nd Edition p. 355
     @author P. Conrad (who only typed it in and added the Javadoc comments)
     @author N. Duncan
-    @version CS56, Spring 2013, UCSB
+    @version CS56, Fall 2017, UCSB
 */
 
 public class SimpleGui1 {
